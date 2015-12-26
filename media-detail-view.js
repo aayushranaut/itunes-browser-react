487debaf2fecace141df756a2bd0e510453e24e4
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-12-26 10:33:21 +0530

    Create media detail view boiler

---
+++ media-detail-view.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var React = require('react-native');
+
+var {
+  Image,
+  ScrollView,
+  Text,
+  TouchableHighlight,
+  View,
+} = React;
+
+var MediaDetailView = React.createClass({
+  render: function() {
+    return (
+      <View></View>
+    );
+  }
+});
+
+module.exports = MediaDetailView;
