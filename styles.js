dadaa1de5f65b0d3536b214e98d3a9691908d051
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-12-26 10:37:17 +0530

    Added detail view stylesheet boiler

--- styles.js
+++ styles.js
@@ -5,5 +5,6 @@ var { StyleSheet } = React;
 
 module.exports = {
   global: require('./styles/global'),
-  listView: require('./styles/list-view')
+  listView: require('./styles/list-view'),
+  detailView: require('./styles/detail-view'),
 };
---
+++ styles/detail-view.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var React = require('react-native');
+var { StyleSheet} = React;
+
+module.exports = StyleSheet.create({
+  
+});
